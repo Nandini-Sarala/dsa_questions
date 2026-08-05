9d7b691f88fb7c1cacb589703ec536ec565de144
Nandini Sarala <201964015+Nandini-Sarala@users.noreply.github.com>
2026-08-05 19:59:06 +0530

    Time: 168 ms (5.08%) | Memory: 23.9 MB (19.83%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left=0;
+        for(int right=0;right<nums.size();right++){
+            if(nums[right]!=0){
+                int temp=nums[right];
+                nums[right]=nums[left];
+                nums[left]=temp;
+                left++;
+                
+
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            cout<<nums[i]<<" ";
+        }
+        
+    }
+};
