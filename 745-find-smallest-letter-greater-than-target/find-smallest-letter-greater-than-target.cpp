37bd002645edae783555cd047eab42d044e45df9
Nandini Sarala <201964015+Nandini-Sarala@users.noreply.github.com>
2026-08-21 20:31:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (23.92%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,50 +1,38 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-    //     int st=0;
-    //     int end=letters.size()-1;
-    //    // int mid;
-    //     if(int(target)>=int(letters[end]))
-    //         return letters[0];
-    //     else{
-    //     while(st<=end ){
-    //         int mid=st+(end-st)/2;
-    //         if(int(target)<int(letters[mid])){
-    //             end=mid-1;
-    //         }
-    //         else if(int(target)>int(letters[mid])){
-    //             st=mid+1;
-    //         }
-    //         else{
-    //             if(letters[mid]==letters[mid+1]){
-    //                 mid++;
-    //             }else 
-    //              return letters[mid+1];
-    //         }
-    //     }
-            
-            // return char(int(letters[mid+1]));
-            // return letters[end];
-            //if()
-        //     //return letters[end+1];
+    int st=0;
+        int end=letters.size()-1;
+       // int mid;
+        // if(int(target)>=int(letters[end])){
+        //    // cout<<letters[0];
+        //     return letters[0];
         // }
-        
-        // return letters[st];
-        int st = 0, end = letters.size() - 1;
-    int ansIdx = -1;
-
-    while (st <= end) {
-        int mid = st + (end - st) / 2;
-        if (letters[mid] > target) {
-            ansIdx = mid;      // candidate answer, keep looking left for a smaller one
-            end = mid - 1;
-        } else {
-            st = mid + 1;      // letters[mid] <= target, need to look right
+            
+        // else{
+        while(st<=end ){
+            int mid=st+(end-st)/2;
+            if(int(target)<int(letters[mid])){
+                end=mid-1;
+            }
+            // else if(int(target)>int(letters[mid])){
+            //     st=mid+1;
+            // }
+            else 
+            {
+                st=mid+1;
+            }
+            // else{
+               
+            //    // cout<<letters[mid+1];
+            //     return letters[mid+1];
+            // }
         }
-    }
-
-   // cout << (ansIdx == -1 ? letters[0] : letters[ansIdx]) << endl;
-    return (ansIdx == -1 ? letters[0] : letters[ansIdx]);
+            
+           
+       // }
+       // cout<<letters[st];
+        return letters[st%letters.size()];
 
         
     }
